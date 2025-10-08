f1bd74a5d68d6f6eca79c0284a468b1cd813349b
YuriUrri <urrijehan1@gmail.com>
2025-10-09 07:29:11 +0800

    LoginPage implementation
    -Login

--- KusinApp/LoginPage.Designer.cs
+++ KusinApp/LoginPage.Designer.cs
@@ -28,12 +28,122 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "LoginPage";
+            label1 = new Label();
+            label2 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            label4 = new Label();
+            label5 = new Label();
+            label3 = new Label();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(181, 110);
+            label1.Name = "label1";
+            label1.Size = new Size(0, 32);
+            label1.TabIndex = 0;
+            label1.Click += this.label1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Gill Sans MT", 36F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.ImageAlign = ContentAlignment.TopCenter;
+            label2.Location = new Point(71, 62);
+            label2.Margin = new Padding(6, 0, 6, 0);
+            label2.Name = "label2";
+            label2.Size = new Size(492, 133);
+            label2.TabIndex = 1;
+            label2.Text = "KusinApp";
+            label2.Click += label2_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(153, 494);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(267, 39);
+            textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(154, 616);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(267, 39);
+            textBox2.TabIndex = 4;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(153, 581);
+            label4.Name = "label4";
+            label4.Size = new Size(111, 32);
+            label4.TabIndex = 5;
+            label4.Text = "Password";
+            label4.Click += label4_Click;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(153, 459);
+            label5.Name = "label5";
+            label5.Size = new Size(121, 32);
+            label5.TabIndex = 6;
+            label5.Text = "Username";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 20F);
+            label3.Location = new Point(154, 282);
+            label3.Name = "label3";
+            label3.Size = new Size(266, 72);
+            label3.TabIndex = 7;
+            label3.Text = "Welcome!";
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.FromArgb(255, 128, 0);
+            button1.BackgroundImageLayout = ImageLayout.Zoom;
+            button1.Location = new Point(211, 711);
+            button1.Name = "button1";
+            button1.Size = new Size(150, 46);
+            button1.TabIndex = 8;
+            button1.Text = "Login";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // LoginPage
+            // 
+            AutoScaleDimensions = new SizeF(13F, 32F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ButtonHighlight;
+            ClientSize = new Size(639, 1259);
+            Controls.Add(button1);
+            Controls.Add(label3);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "LoginPage";
+            Text = "LoginPage";
+            Load += LoginPage_Load;
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private Label label2;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Label label4;
+        private Label label5;
+        private Label label3;
+        private Button button1;
     }
 }
--- KusinApp/LoginPage.cs
+++ KusinApp/LoginPage.cs
@@ -16,5 +16,23 @@ namespace KusinApp
         {
             InitializeComponent();
         }
+
+        private void LoginPage_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+        }
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
